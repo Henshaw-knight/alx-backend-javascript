986d4e53cf3880a8a95f4ba200548092189dbc7e
Henshaw <josephhensh2000@gmail.com>
2024-05-02 00:48:51 -0700

    Update file

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,2 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = undefined, expansion2019 = undefined) {
-  return (initialNumber + (expansion1989 = expansion1989 || 89) + (expansion2019 = expansion2019 || 19));}
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return (initialNumber + expansion1989 + expansion2019);
+}
