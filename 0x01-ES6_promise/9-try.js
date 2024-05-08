68994a830734b82e356f3238bdd0b501a4b7cbff
Henshaw <josephhensh2000@gmail.com>
2024-05-08 08:18:48 -0700

    Update file

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,9 +3,9 @@ export default function guardrail(mathFunction) {
   try {
     const result = mathFunction();
     queue.push(result);
-    queue.push('Guardrial was processed');
   } catch (error) {
-    queue.push(`${error}`);
+    queue.push(`Error: ${error.message}`);
+  } finally {
     queue.push('Guardrial was processed');
   }
   return queue;
