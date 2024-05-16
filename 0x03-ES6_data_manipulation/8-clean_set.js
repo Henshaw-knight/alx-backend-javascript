6f7117b1ceb70dbccc7e603614bcc889890bf5c5
Henshaw <josephhensh2000@gmail.com>
2024-05-16 01:27:38 -0700

    Update files

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,8 @@
 export default function cleanSet(set, startString) {
   const strArr = [];
   for (const value of set) {
-    if (value.startsWith(startString) && startString.length !== 0) {
+    if (value && value.startsWith(startString)
+      && startString.length !== 0 && typeof startString === 'string') {
       const remainingPart = value.substring(startString.length);
       strArr.push(remainingPart);
     }
