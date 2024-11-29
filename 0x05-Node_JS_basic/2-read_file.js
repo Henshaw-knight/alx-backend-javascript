cf93c174a741d79e6afa8ac42574839f0dfc30bd
Henshaw <josephhensh2000@gmail.com>
2024-11-29 09:53:15 +0000

    Create countStudents function, that reads database file synchronously

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const records = data.split('\n').filter((line) => line.trim() !== '');
+    const count = records.length - 1;
+    const CSRecords = [];
+    const SWERecords = [];
+
+    records.forEach((row) => {
+      const dataArr = row.split(',');
+      if (dataArr[dataArr.length - 1] === 'CS') {
+        CSRecords.push(dataArr[0]); // add firstname to array
+      }
+      if (dataArr[dataArr.length - 1] === 'SWE') {
+        SWERecords.push(dataArr[0]);
+      }
+    });
+
+    console.log(`Number of students in CS: ${CSRecords.length}. List: ${CSRecords.join(', ')}`);
+    console.log(`Number of students in SWE: ${SWERecords.length}. List: ${SWERecords.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
