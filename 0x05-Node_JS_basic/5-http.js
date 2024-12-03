91c632b158fa4f5ac33b932e1eeaca3237dc5b0b
Henshaw <josephhensh2000@gmail.com>
2024-12-03 06:39:39 +0000

    Create server using http module that listens on port 1245 and displays customized message for different URL paths

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+
+  if (req.url === '/students') {
+    const databaseFile = process.argv[2];
+    countStudents(databaseFile)
+      .then((data) => {
+        const records = data.split('\n').filter((line) => line.trim() !== '');
+        const studentCount = records.length - 1;
+        const CSRecords = [];
+        const SWERecords = [];
+
+        records.forEach((row) => {
+          const dataArr = row.split(',');
+          if (dataArr[dataArr.length - 1] === 'CS') {
+            CSRecords.push(dataArr[0]);
+          }
+          if (dataArr[dataArr.length - 1] === 'SWE') {
+            SWERecords.push(dataArr[0]);
+          }
+        });
+        res.write('This is the list of ours students\n');
+        res.write(`Number of students: ${studentCount}\n`);
+        res.write(`Number of students in CS: ${CSRecords.length}. List: ${CSRecords.join(', ')}\n`);
+        res.write(`Number of students in SWE: ${SWERecords.length}. List: ${SWERecords.join(', ')}`);
+        res.end();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
