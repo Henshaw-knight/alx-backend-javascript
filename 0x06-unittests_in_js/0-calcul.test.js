23699e1c699b9e59c5aa3e0ba247863112f35034
Henshaw <josephhensh2000@gmail.com>
2025-01-23 16:42:35 +0000

    Task 1

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('Sum of two numbers -> calculateNumber', () => {
+  it('checks that a is rounded', () => {
+    assert.strictEqual(calculateNumber(1.98, 2), 4);
+  });
+
+  it('checks that b is rounded', () => {
+    assert.strictEqual(calculateNumber(2, 5.5), 8);
+  });
+
+  it('checks that sum of a and b is a whole number', () => {
+    assert.strictEqual(calculateNumber(3.12, 7.95), 11);
+  });
+});
