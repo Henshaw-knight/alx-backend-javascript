390872ddf8a259ec9d766af618857284436ed3dc
Henshaw <josephhensh2000@gmail.com>
2025-02-02 08:29:24 -0800

    Install chai and work on task 2

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,16 @@
+module.exports = function calculateNumber(type, a, b) {
+  const allowedOperations = ['SUM', 'SUBTRACT', 'DIVIDE'];
+
+  if (!allowedOperations.includes(type)) {
+    throw new Error(`Invalid operation type: ${type}`);
+  }
+  if (Number.isNaN(a) || Number.isNaN(b))
+    throw new TypeError('Arguments must be numbers');
+
+  if (type === 'SUM') return Math.round(a) + Math.round(b)
+  else if (type === 'SUBTRACT') return Math.round(a) - Math.round(b)
+  else if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) return 'Error';
+    return (Math.round(a) / Math.round(b))
+  }
+}
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,66 @@
+const expect  = require('chai').expect;
+const { describe, it, context } = require('mocha');
+const calculateNumber = require('./2-calcul_chai');
+
+describe('Arithmetic operations on two numbers -> calculateNumber', () => {
+  context("type === 'SUM'", () => {
+    it('checks that positive numbers are rounded and added correctly', () => {
+      expect(calculateNumber('SUM', 5.0, 7.5)).to.equal(13);
+      expect(calculateNumber('SUM', 2.8, 3.2)).to.equal(6);
+      expect(calculateNumber('SUM', 10.7, 4.8)).to.equal(16);
+    });
+
+    it('checks that negative numbers are rounded and added correctly', () => {
+      expect(calculateNumber('SUBTRACT', -4.2, -8.6)).to.equal(5);
+      expect(calculateNumber('SUBTRACT', -1.6, -3.0)).to.equal(1);
+      expect(calculateNumber('SUBTRACT', -11.53, -2.8)).to.equal(-9);
+    });
+
+    it('checks that positive and negative numbers are added correctly', () => {
+      expect(calculateNumber('SUM', 9.2, -3.8)).to.equal(5);
+      expect(calculateNumber('SUM', -5.6, 5.1)).to.equal(-1);
+    }) 
+  })
+
+  context("type === 'SUBTRACT'", () => {
+    it('checks that subtraction on positive numbers work correctly', () => {
+      expect(calculateNumber('SUBTRACT', 7.0, 2.5)).to.equal(4);
+      expect(calculateNumber('SUBTRACT', 2.8, 3.2)).to.equal(0);
+      expect(calculateNumber('SUBTRACT', 10.7, 4.8)).to.equal(6);
+    });
+
+    it('checks that subtraction on negative numbers work correctly', () => {
+      expect(calculateNumber('SUBTRACT', -4.2, -8.6)).to.equal(5);
+      expect(calculateNumber('SUBTRACT', -5.6, -3.0)).to.equal(-3);
+    });
+
+    it('checks that positive and negative numbers are added correctly', () => {
+      expect(calculateNumber('SUBTRACT', 9.2, -3.8)).to.equal(13);
+      expect(calculateNumber('SUBTRACT', -5.6, 5.1)).to.equal(-11);
+    }) 
+  })
+  
+  context('type === "DIVIDE"', () => {
+    it('checks that positive numbers are divided correctly', () => {
+      expect(calculateNumber('DIVIDE', 8.2, 2.0)).to.eqaul(4);
+    });
+
+    it('checks that negative numbers are divided correctly', () => {
+      expect(calculateNumber('DIVIDE', -5.6, -3.2)).to.equal(2);
+    });
+
+    it('checks that positive and negative numbers are divided correctly', () => {
+      expect(calculateNumber('DIVIDE', 8.2, -2.0)).to.eqaul(-4);
+      expect(calculateNumber('DIVIDE', -5.6, 3.2)).to.equal(-2);
+    });
+});
+  context ('More edge cases', () => {
+    it('should throw an error for invalid operations', () => {
+      expect(() => calculateNumber('MULTIPLY', 10, 5)).to.throw('Invalid operation');
+  });
+
+  it('should throw an error when dividing by zero', () => {
+    expect(() => calculateNumber('DIVIDE', 10, 0)).to.throw('Error');
+  });
+});
+});
